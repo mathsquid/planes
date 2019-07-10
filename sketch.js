6a4fa8143d0224bb7e7d2ecb79a397f3bbb51ed9
mathsquid <dleach121@gmail.com>
2019-07-10 11:35:00 -0400

    added 4th plane

--- sketch.js
+++ sketch.js
@@ -4,29 +4,34 @@ one = 100;
 ticksize=4;
 
 let pg;
-  // each p holds parameters for a Plane
-  // 0 - a
-  // 1 - b
-  // 2 - c
-  // 3 - d
-  // 4 - opacity
+// each p holds parameters for a Plane
+// 0 - a
+// 1 - b
+// 2 - c
+// 3 - d
+// 4 - opacity
 
-let p = [[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1]];
-let colorArray = [[255,0,255], [0,255,0], [0,0,255]];
+let p = [[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1]];
+let colorArray = [[255,0,255], [0,255,0], [0,0,255], [255,0,0]];
 
 function setup() {
   createCanvas(WIDTH,HEIGHT, WEBGL);
-  // normalMaterial();
-  // slider1 = createSlider(0, 50, 15);
-  // slider2 = createSlider(0, 50, 15);
-  // slider3 = createSlider(0, 10000, one*15);
-  updateParameters();
+  //look at xy-plane
+   camera(0, 0, (HEIGHT/2.0) / tan(PI*30.0 / 180.0), 0, 0, 0, 0,1,0);
+  // look at xz-plane
+   // camera(0, (HEIGHT/2.0) / tan(PI*30.0 / 180.0), 0,
+   //        0, 0, 0,
+   //        0, 0 ,-1);
+
+
+
+    updateParameters();
   // img = loadImage('garfield.jpg');
 }
 
 
 function drawAxes(){
-// x axis
+  // x axis
   strokeWeight(3);
   stroke(255,0,0);
   push();
@@ -45,7 +50,7 @@ function drawAxes(){
   stroke(255,0,0);
   line(-WIDTH,0,0,WIDTH,0,0);
 
-// y axis
+  // y axis
   stroke(0);
   push();
   for (i=0; i<WIDTH/2; i+=one){
@@ -95,6 +100,10 @@ function updateParameters(){
   p[2][1]=math.parse(document.getElementById("p2b").value).compile().eval();
   p[2][2]=math.parse(document.getElementById("p2c").value).compile().eval();
   p[2][3]=math.parse(document.getElementById("p2d").value).compile().eval();
+  p[3][0]=math.parse(document.getElementById("p3a").value).compile().eval();
+  p[3][1]=math.parse(document.getElementById("p3b").value).compile().eval();
+  p[3][2]=math.parse(document.getElementById("p3c").value).compile().eval();
+  p[3][3]=math.parse(document.getElementById("p3d").value).compile().eval();
 }
 
 function draw() {
@@ -108,49 +117,49 @@ function draw() {
   planeSize = document.getElementById("planeSize").value;
 
 
-// draw the three planes
-for (i=0; i<3; i++){
-  i1 = "p"+i+"show";
-  i2 = "p"+i+"opacity";
-  if (document.getElementById(i1).checked){
-    // this needs work
-    equation = "      "+p[i][0]+"x + "+p[i][1]+"y + "+p[i][2]+"z"+" = "+p[i][3];
-    document.getElementById("planetext"+i).innerHTML=equation;
-    var op  = math.parse(document.getElementById(i2).value).compile().eval();
-    push();
-    if(p[i][0]==0 && p[i][2]==0 && p[i][1]==0){
+  // draw the three planes
+  for (i=0; i<4; i++){
+    i1 = "p"+i+"show";
+    i2 = "p"+i+"opacity";
+    if (document.getElementById(i1).checked){
+      // this needs work
+      equation = "      "+p[i][0]+"x + "+p[i][1]+"y + "+p[i][2]+"z"+" = "+p[i][3];
+      document.getElementById("planetext"+i).innerHTML=equation;
+      var op  = math.parse(document.getElementById(i2).value).compile().eval();
+      push();
+      if(p[i][0]==0 && p[i][2]==0 && p[i][1]==0){
+        // Do Nothing
+      }
+      else if (p[i][0]==0 && p[i][2]==0){
+        rotateX(PI/2);
+        translate(0,0,one*p[i][3]/p[i][1]);
+        ambientMaterial(colorArray[i][0],colorArray[i][1],colorArray[i][2],op);
+        plane(15*one,15*one,2,2);
+      }
+      else{
+        //--------------------------------------------------------------------------
+        //--------------------------------------------------------------------------
+        //--------------------------------------------------------------------------
+        k = p[i][3]/sqrt(p[i][0]*p[i][0] + p[i][2]*p[i][2]);
+        rotateY(atan(p[i][0]/p[i][2]));
+        // rotateY(frameCount/1000);
+        if (document.getElementById("debug").checked){
+          translate(0,0,k*one);
+        }
+        else {
+          translate(0,0,-k*one);
+        }
+        rotateX(PI/2-atan( sqrt(p[i][0]*p[i][0] + p[i][2]*p[i][2]) / p[i][1]) );
+        // rotateX(PI/2-atan(p[i][3]/p[i][1]/k));
+        ambientMaterial(colorArray[i][0],colorArray[i][1],colorArray[i][2],op);
+        plane(planeSize,planeSize,2,2);
+        //--------------------------------------------------------------------------
+        //--------------------------------------------------------------------------
+        //--------------------------------------------------------------------------
+      }
+      pop();
     }
-    else if (p[i][0]==0 && p[i][2]==0){
-      rotateX(PI/2);
-      translate(0,0,one*p[i][3]/p[i][1]);
-      ambientMaterial(colorArray[i][0],colorArray[i][1],colorArray[i][2],op);
-      plane(15*one,15*one,2,2);
-    }
-    else{
-      //--------------------------------------------------------------------------
-      //--------------------------------------------------------------------------
-      //--------------------------------------------------------------------------
-      k = p[i][3]/sqrt(p[i][0]*p[i][0] + p[i][2]*p[i][2]);
-      rotateY(atan(p[i][0]/p[i][2]));
-      // rotateY(frameCount/1000);
-      if (document.getElementById("debug").checked){
-      translate(0,0,k*one);
-}
-else {
-  translate(0,0,-k*one);
-
-}
-      rotateX(PI/2-atan( sqrt(p[i][0]*p[i][0] + p[i][2]*p[i][2]) / p[i][1]) );
-      // rotateX(PI/2-atan(p[i][3]/p[i][1]/k));
-      ambientMaterial(colorArray[i][0],colorArray[i][1],colorArray[i][2],op);
-      plane(planeSize,planeSize,2,2);
-      //--------------------------------------------------------------------------
-      //--------------------------------------------------------------------------
-      //--------------------------------------------------------------------------
-    }
-    pop();
   }
-}
 
 
 
